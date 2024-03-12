97a999aca8f20b6c32652bfa3b876d2d57fd6578
KACPER SINILO <kacperosinilo@gmail.com>
2024-03-12 19:45:06 +0100

    update: logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -15,7 +15,7 @@ function Logo() {
          >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
          </svg>
-         Acme Inc
+         Newsletter Master
       </Link>
    )
 }
